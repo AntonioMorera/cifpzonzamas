403cc49a1626aeb24138142d7322b170cd493f40
Juanra <juan_curbelo@cifpzonzamas.es>
2026-01-28 09:12:53 +0000

    FIX: seeder con datos de libro

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Datos;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\LibroController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -22,3 +26,19 @@ require __DIR__.'/auth.php';
 Route::get('/admin', function () {
     return view('admin.dashboard');
 })->middleware('auth');
+
+
+
+
+Route::get('/libro', [LibroController::class, 'index'])->name('libro.index');
+Route::get('/libro/create', [LibroController::class, 'create'])->name('libro.create');
+Route::post('/libro/create', [LibroController::class, 'create'])->name('libro.create');
+
+Route::get('/libro/edit/{i}', [LibroController::class, 'edit'])->name('libro.edit');
+Route::post('/libro/edit', [LibroController::class, 'edit'])->name('libro.edit');
+
+Route::get('/libro/show/{i}', [LibroController::class, 'show'])->name('libro.show');
+
+
+Route::get('/libro/destroy/{i}', [LibroController::class, 'destroy'])->name('libro.destroy');
+Route::post('/libro/destroy', [LibroController::class, 'destroy'])->name('libro.destroy');
